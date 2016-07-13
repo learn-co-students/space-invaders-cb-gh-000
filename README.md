c47340ffebf86a03dd2f71b97b2a540de8485eec
Scott C Reynolds <scott@scottcreynolds.com>
2016-07-13 15:52:45 -0400

    update lab tests, structure, classes (#11)
    * update lab tests, structure, classes
    
    * update .learn
    
    * update package.json

--- README.md
+++ README.md
@@ -1,17 +1,16 @@
 # Space Invaders
-## Instructions:
 
-Space flight is no joke, and neither is this lab.  You're going to need to create two JS constructors, `Spaceship` and `CrewMember`. These constructors will pump out instances that are by default unable to interact with one another.  A space ship will be inactive if it is created without a crew.  And a crew member by default will be hanging out in the Cantina looking for a ride if they were not put inside a ship when it is initialized.
+## Instructions
+
+Space flight is no joke, and neither is this lab.  You're going to need to create two ES6 JS classes, `Spaceship` and `CrewMember`. These classes will pump out instances that are by default unable to interact with one another.  A space ship will be inactive if it is created without a crew.  And a crew member by default will be hanging out in the Cantina looking for a ride if they were not put inside a ship when it is initialized.
 
 Crew members will also be unable to use their special abilities if they're not assigned to a ship.  For example, a pilot cannot `engageWarpDrive` if he is not in a ship and, therefore, can't make the Kessel Run in less than twelve parsecs. Crew members also should be unable to perform certain actions that their positions are incapable of doing.
 
 To have these two object types be aware of one another we need to have the ship become aware of it's crew members when it's created. This will also require you to make sure a crew member becomes aware of their ship when they are added to it.
 
-To run the specs follow these commands:
-- to run in the command line run
+- To run tests on the command line run
   -  `learn`
-- to run in the browser
-  - `learn -b`
 
 <p data-visibility='hidden'>View <a href='https://learn.co/lessons/space-invaders' title='Space Invaders'>Space Invaders</a> on Learn.co and start learning to code for free.</p>
 
+<p data-visibility='hidden'>View <a href='https://learn.co/lessons/space-invaders'>JS Space Invaders Lab</a> on Learn.co and start learning to code for free.</p>
---
+++ test/space-invaders-test.js
@@ -0,0 +1,120 @@
+const expect = require('expect')
+
+const fs = require('fs')
+const jsdom = require('mocha-jsdom')
+const path = require('path')
+const babel = require('babel-core')
+
+const babelSpaceship = babel.transformFileSync(path.resolve(__dirname, '..', 'spaceship.js'), {
+  presets: ['es2015']
+})
+const babelCrewMember = babel.transformFileSync(path.resolve(__dirname, '..', 'crewMember.js'), {
+  presets: ['es2015']
+})
+
+describe('space invaders', () => {
+
+  jsdom({
+    src: babelSpaceship.code + babelCrewMember.code
+  })
+
+  describe('CrewMember', function() {
+    let tristan, jon, katie
+    beforeEach(function() {
+      tristan = new CrewMember('Pilot')
+      jon = new CrewMember('Defender')
+      katie = new CrewMember('Gunner')
+    })
+
+    it('should know their position', function() {
+      expect(tristan.position).toBe('Pilot')
+      expect(jon.position).toBe('Defender')
+      expect(katie.position).toBe('Gunner')
+    })
+
+    it('should should return "Looking for a Rig" if they aren\'t assigned to a ship', function() {
+      expect(tristan.currentShip).toBe('Looking for a Rig')
+    })
+
+    it('should return "had no effect" when the crew member tries to use their special ability', function() {
+      expect(tristan.engageWarpDrive()).toBe('had no effect')
+      expect(jon.setsInvisibility()).toBe('had no effect')
+      expect(katie.chargePhasers()).toBe('had no effect')
+    })
+  })
+
+  describe('Spaceship', function() {
+    var spaceship
+    beforeEach(function() {
+      spaceship = new Spaceship('The Krestel', [], 5, 4)
+    })
+
+    it('should know its own name', function() {
+      expect(spaceship.name).toBe('The Krestel')
+    })
+
+    it('should have the correct number of phasers (5)', function() {
+      expect(spaceship.phasers).toBe(5)
+    })
+
+    it('should have the correct layer of shields (4)', function() {
+      expect(spaceship.shields).toBe(4)
+    })
+
+    it('should have its cloaking set to false by default', function() {
+      expect(spaceship.cloaked).toBe(false)
+    })
+
+    it('should have its warp drive set to "disengaged" by default', function() {
+      expect(spaceship.warpDrive).toBe('disengaged')
+    })
+
+    it('should be docked if it has no crew', function() {
+      expect(spaceship.docked).toBe(true)
+    })
+
+    it('should have its `phasers` charge set to "uncharged" by default', function() {
+      expect(spaceship.phasersCharge).toBe('uncharged')
+    })
+  })
+
+  describe('Ship with a crew', function() {
+    var tristan, jon, katie, spaceship
+    beforeEach(function() {
+      tristan = new CrewMember('Pilot')
+      jon = new CrewMember('Defender')
+      katie = new CrewMember('Gunner')
+      spaceship = new Spaceship('The Krestel', [tristan, jon, katie], 5, 4)
+    })
+
+    it('`docked` should return false for ships with a crew', function() {
+      expect(spaceship.docked).toBe(false)
+    })
+
+    it('a crew member should return their ship when `currentShip` is called on them', function() {
+      expect(tristan.currentShip).toBe(spaceship)
+      expect(tristan.currentShip.name).toBe('The Krestel')
+    })
+
+    it('should charge its phasers when a gunner calls `chargePhasers`', function() {
+      tristan.chargePhasers()
+      expect(spaceship.phasersCharge).toBe('uncharged')
+      katie.chargePhasers()
+      expect(spaceship.phasersCharge).toBe('charged!')
+    })
+
+    it('should have its warp drive set to "engaged" only when the pilot uses `engageWarpDrive`', function() {
+      jon.engageWarpDrive()
+      expect(spaceship.warpDrive).toBe('disengaged')
+      tristan.engageWarpDrive()
+      expect(spaceship.warpDrive).toBe('engaged!')
+    })
+
+    it('should cloak when a defender `setsInvisibility`', function() {
+      katie.setsInvisibility()
+      expect(spaceship.cloaked).toBe(false)
+      jon.setsInvisibility()
+      expect(spaceship.cloaked).toBe(true)
+    })
+  })
+})
