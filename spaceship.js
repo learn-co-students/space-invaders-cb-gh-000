a802aa93240dd32fef27bcedce81125af0588b34
github username <andrewcohn0@gmail.com>
2017-12-13 14:52:42 -0500

    add solution; test refactor next

---
+++ spaceship.js
@@ -0,0 +1,29 @@
+// unique properties and methods go inside the constructor
+class Spaceship {
+  constructor(name, crew, phasers, shields) {
+    this.name = name;
+    this.crew = crew;
+    this.phasers = phasers;
+    this.shields = shields;
+    this.cloaked = false;
+    this.warpDrive = "disengaged";
+    if (crew.length === 0) {
+      this.docked = true;
+    } else {
+      this.docked = false;
+    }
+    this.phasersCharge = "uncharged";
+    this.notifyCrew();
+  }
+
+  notifyCrew() {
+    // using an arrow function lexically
+    // binds `this` to the context of this
+    // instance of Spaceship, rather than
+    // the context of the callback
+    this.crew.forEach(crewMember => {
+      crewMember.currentShip = this;
+    });
+  }
+}
+
