fed39c83844810702a0ecf28a58382986d99234c
github username <andrewcohn0@gmail.com>
2017-12-13 14:51:31 -0500

    tests successfully run in browser; just need to rewrite tests && run against solution

--- test/crewMemberTest.js
+++ test/crewMemberTest.js
@@ -1,3 +1,5 @@
+const expect = chai.expect;
+
 describe("crewMember.js", () => {
   it("should work when I run learn", () => {
     expect(2).to.eq(2);
---
+++ test/spaceshipTest.js
@@ -0,0 +1,7 @@
+// const expect = chai.expect;
+
+describe("spaceShip.js", () => {
+  it("should work when I run learn", () => {
+    expect(2).to.eq(2);
+  });
+});
