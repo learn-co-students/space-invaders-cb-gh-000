ace9474e2dbaf9b9702e5e22181cfdb488450aed
maxwellbenton <maxwellbenton@gmail.com>
2019-01-14 11:35:02 -0500

    updates solution

--- src/crewMember.js
+++ src/crewMember.js
@@ -1,1 +1,24 @@
-class CrewMember {}
+class CrewMember {
+  constructor( position ) {
+    this.position = position;
+    this.currentShip = 'Looking for a Rig';
+  }
+
+  engageWarpDrive() {
+    return this.position === 'Pilot' && this.currentShip !== 'Looking for a Rig' ?
+      ( this.currentShip.warpDrive = 'engaged' ) :
+      'had no effect';
+  }
+
+  setsInvisibility() {
+    return this.position === 'Defender' && this.currentShip !== 'Looking for a Rig' ?
+      ( this.currentShip.cloaked = true ) :
+      'had no effect';
+  }
+
+  chargePhasers() {
+    return this.position === 'Gunner' && this.currentShip !== 'Looking for a Rig' ?
+      ( this.currentShip.phasersCharge = 'charged' ) :
+      'had no effect';
+  }
+}
--- src/spaceship.js
+++ src/spaceship.js
@@ -1,1 +1,17 @@
-class Spaceship {}
+class Spaceship {
+  constructor( name, crew, phasers, shields ) {
+    this.name = name;
+    this.crew = crew;
+    this.phasers = phasers;
+    this.shields = shields;
+    this.cloaked = false;
+    this.warpDrive = 'disengaged';
+    this.phasersCharge = 'uncharged';
+    crew.length ? ( this.docked = false ) : ( this.docked = true );
+    this.notifyCrew();
+  }
+
+  notifyCrew() {
+    this.crew.forEach( crewMember => ( crewMember.currentShip = this ) );
+  }
+}
